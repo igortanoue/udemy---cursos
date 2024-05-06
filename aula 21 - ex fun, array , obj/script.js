a81421e539a90a51e65ac5a7e527975994ce2ddb
igortanoue <igortanoue@gmail.com>
2024-05-06 00:45:30 -0300

    Create script.js

---
+++ aula 21 - ex fun, array , obj/script.js
@@ -0,0 +1,9 @@
+function enviar(nome = '', sobrenome = '', peso = 0 , altura = 0){
+    let pessoa1 = {
+        nome: document.getElementById('nome'),
+        sobrenome: document.getElementById('sobrenome'),
+        peso: document.getElementById('peso'),
+        altura: document.getElementById('altura')
+    }
+    res.innerHTML += `${pessoa1}`
+}
